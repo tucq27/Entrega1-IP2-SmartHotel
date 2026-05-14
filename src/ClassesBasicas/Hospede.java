65b14e23f5ee3d08cea37b0be41a9ec68d2fa950
Gabriela <couceirog2.0@gmail.com>
2026-05-14 11:54:55 -0300

    Atualizar o Hospede.java

--- src/ClassesBasicas/Hospede.java
+++ src/ClassesBasicas/Hospede.java
@@ -1,29 +1,46 @@
 package ClassesBasicas;
 
+// Classe Hospede herda atributos e métodos da classe Pessoa
 public class Hospede extends Pessoa {
+
+    // Preferências do hóspede
+    // Exemplo: quarto com varanda, cama de casal etc.
     private String preferencias;
+
+    // Histórico do hóspede no hotel
+    // Pode guardar informações sobre hospedagens anteriores
     private String historico;
 
+    // Construtor da classe Hospede
     public Hospede(String nome, String cpf, java.time.LocalDate dataNascimento,
                    String telefone, String email,
                    String preferencias, String historico) {
+
+        // Chama o construtor da classe Pessoa
+        // para inicializar os dados básicos
         super(nome, cpf, dataNascimento, telefone, email);
+
+        // Inicializa os atributos específicos de Hospede
         this.preferencias = preferencias;
         this.historico = historico;
     }
 
+    // Retorna as preferências do hóspede
     public String getPreferencias() {
         return preferencias;
     }
 
+    // Altera as preferências do hóspede
     public void setPreferencias(String preferencias) {
         this.preferencias = preferencias;
     }
 
+    // Retorna o histórico do hóspede
     public String getHistorico() {
         return historico;
     }
 
+    // Altera o histórico do hóspede
     public void setHistorico(String historico) {
         this.historico = historico;
     }
